dd5f7152325e25a3c683ca3d26a4a3117e0f5a60
Tshepo <mbowenitshepo@gmail.com>
2018-09-25 13:10:18 +0200

    read me

--- resources/views/admin/adminlayout.blade.php
+++ resources/views/admin/adminlayout.blade.php
@@ -135,7 +135,7 @@
         <footer class="main-footer">
             <!-- To the right -->
             <!-- Default to the left -->
-            <strong>Copyright &copy; {{ date('Y') }} <a href="#">TJ MBOWENI</a>.</strong> All rights reserved.
+            
         </footer>
 
     </div>
